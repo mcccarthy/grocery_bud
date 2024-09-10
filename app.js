96b9d18a4a8650c5f216369806d0ef5113f516a2
James Mccarthy <mccarthyjames35@yahoo.com>
2024-09-10 11:45:45 -0700

    added logo and css

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 // ****** SELECT ITEMS **********
-
+const alert = document.querySelector('.alert');
+const form = document.querySelector('.grocery-form');
+const grocery = document.getElementById('grocery');
+const container = document.querySelector('.grocery-container');
+const submitBtn = document.querySelector('.submit-btn');
+const list = document.querySelector('.grocery-list');
+const clearBtn = document.querySelector('.clear-btn');
 // edit option
+let editElement;
+let editFlag = false;
+let editID = '';
 
 // ****** EVENT LISTENERS **********
 
